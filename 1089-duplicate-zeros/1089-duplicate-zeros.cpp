e5687e870ddd28eab556e2614470df22b26c64d8
Tekulapalli Abhiram <85804287+abhiram-tekulapalli@users.noreply.github.com>
2026-07-07 20:45:30 +0530

    Time: 0 ms (100%), Space: 13.9 MB (74.82%) - LeetHub

---
+++ 1089-duplicate-zeros/1089-duplicate-zeros.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    void duplicateZeros(vector<int>& arr) {
+        int n=arr.size();
+        int zeroes=0;
+        for(int num : arr){
+            if(num==0)zeroes++;
+        }
+        int i  =n-1;
+        int j = n+zeroes-1;
+        while(i>=0){
+            if(j<n){
+                arr[j]=arr[i];
+            }
+            if(arr[i]==0){
+                j--;
+                if(j<n){
+                    arr[j]=0;
+                }
+            }
+            i--;
+            j--;
+        }
+    }
+};
